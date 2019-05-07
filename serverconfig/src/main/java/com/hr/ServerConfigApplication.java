7f8cdbf5c5b2caaf3718a049120627559be911f8
BUCHU <710625937@qq.com>
2019-05-07 20:25:57 +0800

    手动刷新需要配置

---
+++ serverconfig/src/main/java/com/hr/ServerConfigApplication.java
@@ -0,0 +1,23 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 胡冉
+ * @ClassName ServerConfigApplication
+ * @Date 2019/5/7 15:34
+ * @Version 2.0
+ */
+/**
+ * 开启配置服务
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ServerConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerConfigApplication.class);
+    }
+}
