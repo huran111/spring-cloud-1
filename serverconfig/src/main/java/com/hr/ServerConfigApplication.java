3864cb899196ff227ad9e1921fe5341b6cb308e4
BUCHU <710625937@qq.com>
2019-05-09 19:16:01 +0800

    添加网关zuul

--- serverconfig/src/main/java/com/hr/ServerConfigApplication.java
+++ serverconfig/src/main/java/com/hr/ServerConfigApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 /**
  * 开启配置服务
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ServerConfigApplication {
